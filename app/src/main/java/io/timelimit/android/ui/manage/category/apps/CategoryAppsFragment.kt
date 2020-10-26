2c30b648e65b17be4f8c166f790f3d13631f04b2
Jonas Lochmann <git@inkompetenz.org>
2020-10-26 01:00:00 +0100

    Use CategoryAppsAndRulesFragment for CategoryAppsFragment

--- app/src/main/java/io/timelimit/android/ui/manage/category/apps/CategoryAppsFragment.kt
+++ app/src/main/java/io/timelimit/android/ui/manage/category/apps/CategoryAppsFragment.kt
@@ -16,27 +16,13 @@
 package io.timelimit.android.ui.manage.category.apps
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.snackbar.Snackbar
-import io.timelimit.android.R
-import io.timelimit.android.data.Database
-import io.timelimit.android.logic.DefaultAppLogic
-import io.timelimit.android.sync.actions.AddCategoryAppsAction
-import io.timelimit.android.sync.actions.RemoveCategoryAppsAction
-import io.timelimit.android.ui.main.ActivityViewModel
-import io.timelimit.android.ui.main.getActivityViewModel
+import androidx.fragment.app.viewModels
 import io.timelimit.android.ui.manage.category.ManageCategoryFragmentArgs
-import io.timelimit.android.ui.manage.category.apps.add.AddCategoryAppsFragment
 import io.timelimit.android.ui.manage.category.appsandrules.AppAndRuleItem
-import kotlinx.android.synthetic.main.fragment_category_apps.*
+import io.timelimit.android.ui.manage.category.appsandrules.CategoryAppsAndRulesFragment
 
-class CategoryAppsFragment : Fragment() {
+class CategoryAppsFragment : CategoryAppsAndRulesFragment() {
     companion object {
         fun newInstance(params: ManageCategoryFragmentArgs): CategoryAppsFragment = CategoryAppsFragment().apply {
             arguments = params.toBundle()
@@ -44,57 +30,14 @@ class CategoryAppsFragment : Fragment() {
     }
 
     private val params: ManageCategoryFragmentArgs by lazy { ManageCategoryFragmentArgs.fromBundle(requireArguments()) }
-    private val database: Database by lazy { DefaultAppLogic.with(requireContext()).database }
-    private val auth: ActivityViewModel by lazy { getActivityViewModel(requireActivity()) }
-    private val model: CategoryAppsModel by lazy {
-        ViewModelProviders.of(this).get(CategoryAppsModel::class.java)
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_category_apps, container, false)
-    }
+    private val model: CategoryAppsModel by viewModels()
+    override val categoryId: String get() = params.categoryId
+    override val childId: String get() = params.childId
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val adapter = AppAdapter()
-
-        adapter.handlers = object: AppAdapterHandlers {
-            override fun onAppClicked(app: AppAndRuleItem.AppEntry) {
-                if (auth.tryDispatchParentAction(
-                        RemoveCategoryAppsAction(
-                                categoryId = params.categoryId,
-                                packageNames = listOf(app.packageName)
-                        )
-                )) {
-                    Snackbar.make(view, getString(R.string.category_apps_item_removed_toast, app.title), Snackbar.LENGTH_SHORT)
-                            .setAction(R.string.generic_undo) {
-                                auth.tryDispatchParentAction(
-                                        AddCategoryAppsAction(
-                                                categoryId = params.categoryId,
-                                                packageNames = listOf(app.packageName)
-                                        )
-                                )
-                            }
-                            .show()
-                }
-            }
-
-            override fun onAddAppsClicked() {
-                if (auth.requestAuthenticationOrReturnTrueAllowChild(childId = params.childId)) {
-                    AddCategoryAppsFragment.newInstance(
-                            childId = params.childId,
-                            categoryId = params.categoryId,
-                            childAddLimitMode = !auth.isParentAuthenticated()
-                    ).show(parentFragmentManager)
-                }
-            }
-        }
-
-        recycler.layoutManager = LinearLayoutManager(requireContext())
-        recycler.adapter = adapter
-
         model.init(params)
-        model.appEntries.observe(viewLifecycleOwner, Observer { adapter.data = it })
+        model.appEntries.observe(viewLifecycleOwner) { setListContent(it + listOf(AppAndRuleItem.AddAppItem)) }
     }
 }
