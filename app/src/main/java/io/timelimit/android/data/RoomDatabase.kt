ioridev/opentimelimit-mirror

Add preblocking

app/src/main/java/io/timelimit/android/data/RoomDatabase.kt
@@ -48,7 +48,7 @@ import java.util.concurrent.TimeUnit
     UserLimitLoginCategory::class,
     CategoryNetworkId::class,
     ChildTask::class
-], version = 20)
+], version = 21)
 abstract class RoomDatabase: RoomDatabase(), io.timelimit.android.data.Database {
     companion object {
         private val lock = Object()
@@ -102,7 +102,8 @@ abstract class RoomDatabase: RoomDatabase(), io.timelimit.android.data.Database 
                             DatabaseMigrations.MIGRATE_TO_V17,
                             DatabaseMigrations.MIGRATE_TO_V18,
                             DatabaseMigrations.MIGRATE_TO_V19,
-                            DatabaseMigrations.MIGRATE_TO_V20
+                            DatabaseMigrations.MIGRATE_TO_V20,
+                            DatabaseMigrations.MIGRATE_TO_V21
                     )
                     .setQueryExecutor(Threads.database)
                     .build()

mmguero-android/timelimit-android

app/src/main/java/io/timelimit/android/data/RoomDatabase.kt
@@ -50,7 +50,7 @@ import java.util.concurrent.TimeUnit
     UserLimitLoginCategory::class,
     CategoryNetworkId::class,
     ChildTask::class
-], version = 35)
+], version = 36)
 abstract class RoomDatabase: RoomDatabase(), io.timelimit.android.data.Database {
     companion object {
         private val lock = Object()
@@ -119,7 +119,8 @@ abstract class RoomDatabase: RoomDatabase(), io.timelimit.android.data.Database 
                             DatabaseMigrations.MIGRATE_TO_V32,
                             DatabaseMigrations.MIGRATE_TO_V33,
                             DatabaseMigrations.MIGRATE_TO_V34,
-                            DatabaseMigrations.MIGRATE_TO_V35
+                            DatabaseMigrations.MIGRATE_TO_V35,
+                            DatabaseMigrations.MIGRATE_TO_V36
                     )
                     .setQueryExecutor(Threads.database)
                     .build()